d54402bbfaf3afe5e325887778acb969db2171a7
Divya-1110 <64543779+Divya-1110@users.noreply.github.com>
2022-09-29 05:11:36 +0530

    Update BodyMassIndex.java

--- bean/BodyMassIndex.java
+++ bean/BodyMassIndex.java
@@ -4,37 +4,46 @@ import java.util.Scanner;
 
 public class BodyMassIndex {
 
-	public static void main(String[] args) {
-		// get input from user
-				Scanner input =new Scanner(System.in);
-				System.out.println("Enter the Weight in pounds: ");
-				double weight=input.nextDouble();
-				System.out.println("Enter the Height: ");
-				double Height=input.nextDouble();
+	public static void main(String[] args) 
+	{	
+		
+	  Scanner input =new Scanner(System.in);
 				
-	double result=bodyMassIndex(weight,Height);
-	System.out.println("The Bodymassindex is :"+result);
-	System.out.println("The BMI REPORT is :",bmiReport(result));
+	           // get input from user
+		   System.out.println("Enter the Weight in pounds: ");
+		   double weight=input.nextDouble();
+				
+		   System.out.println("Enter the Height: ");
+		   double Height=input.nextDouble();
+	
+		    // display-body mass index
+	            double result=bodyMassIndex(weight,Height);
+	            System.out.printf("The Bodymassindex is :"+result);
+	
+	            //report display using bmi
+	            System.out.println("The BMI REPORT is :"+bmiReport(result));
 	
 	}
-	//bim report
-	private static void bmiReport(double result) {
+	
+	//body mass index report
+	private static String bmiReport(double result) {
 		if(result<19)
 		{
-			System.out.printf("UnderWeight");
+			return "UnderWeight";
 		}
-		else if(result>19) {
-			System.out.printf("Normal Weight");
+		else if(result>55) {
+			return"Normal Weight";
 		}
 		else
 		{
-			System.out.printf("Over Weight");
+			 return"Over Weight";
 		}
-	
+		
 	}
 	
 	//calculating BMI
 	private static double bodyMassIndex(double weight,double Height) {
+		
 		double kilo=weight*0.45359273; //converting pounds to kilogram
 		double meter=Height*0.0254;//converting inches to meters
 		double bmi=kilo/(meter*meter);
