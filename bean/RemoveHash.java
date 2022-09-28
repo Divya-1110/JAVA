32ad325a049535ab42844b249b65f88009e85f54
Divya-1110 <64543779+Divya-1110@users.noreply.github.com>
2022-09-29 05:14:28 +0530

    Update RemoveHash.java

--- bean/RemoveHash.java
+++ bean/RemoveHash.java
@@ -4,24 +4,32 @@ import java.util.Scanner;
 
 public class RemoveHash {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		Scanner input=new Scanner(System.in);
-		//string - user input
 		
+		//string - user input
 	     System.out.println("Enter a string");
 	     String fourthstring=input.next();
 	     
-	     //converting string into a array
-	     
-	     char[] stringarray=fourthstring.toCharArray();
-	      for(int i=0;i<stringarray.length;i++)
-	      {
-	    	if(stringarray[i]=="#") 
-	    	{
-	    		
-	    	}
-	      }
-	     
-
+	       //length of string
+	       int len = fourthstring.length();
+	       
+	       String resultString = "";
+	       
+	       //removing  hash and its immediate left and right values
+	       for (int i = 0; i < len; i++) 
+	       {
+	         if (i == 0 && fourthstring.charAt(i) != '#')
+	            resultString +=fourthstring.charAt(i);
+	           if (i > 0 && fourthstring.charAt(i) != '#' && fourthstring.charAt(i-1) != '#') 
+	           resultString += fourthstring.charAt(i);
+	          if (i > 0 && fourthstring.charAt(i) == '#' && fourthstring.charAt(i-1) != '#') 
+	           resultString = resultString.substring(0,resultString.length()-1);
+	        }
+		
+		//display string after removing hash and its immediate left and right values
+	       System.out.println(resultString);
+    
 	}
 }
