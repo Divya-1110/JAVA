ae443b1b670e9a1504f3b15fb4f5306175b1f221
Divya-1110 <64543779+Divya-1110@users.noreply.github.com>
2022-09-29 05:17:19 +0530

    Update SolutionAreaCircle.java

--- bean/SolutionAreaCircle.java
+++ bean/SolutionAreaCircle.java
@@ -6,24 +6,35 @@ public class SolutionAreaCircle {
 	
     // entry point
 	public static void main(String[] args) {
-		// Test my first program
+	
+           Scanner input =new Scanner(System.in);
+           
+	    // Test my first program
             System.out.println("Welcome!");
+		
             //declare the variables
             double radius;
-            Scanner input =new Scanner(System.in);
-            System.out.print("Enter radius: ");
+		
+	    //user input
+            System.out.print("Enter radius: ");	
             radius=input.nextDouble();
+		
+	   //areaof circle-result
             System.out.println("The radius is : "+areaCircle(radius));// calling method
+	
+	   //circumference of circle result
             System.out.println(String.format("The radius is: %.2f ",Circumference(radius)));        
 	}
 	
 	//calculation method--area
-	private static double areaCircle(double radius){
+	private static double areaCircle(double radius)
+	{
 		return radius*radius*Math.PI;
 	}
 	
 	//circumference
-	private static double Circumference(double radius){
+	private static double Circumference(double radius)
+	{
 		return 2*Math.PI*radius;
 	}
 	
